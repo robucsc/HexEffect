dfb1e83d762315f63acc3b2783ae05bb3941bee3
robucsc <robucsc@gmail.com>
2021-03-04 02:19:33 -0800

    Grid implemented, with some art
    This is with the grid implemented, plus some art, testing out the tiles. Also the mouse script.

---
+++ RoboEffect/Assets/MouseScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    private bool isDragging;
+    private Vector2 difference;
+    
+    public void OnMouseDown(){
+        Debug.Log("mouse down");
+
+        isDragging = true;
+        difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos - difference);
+        }
+    }
+}
