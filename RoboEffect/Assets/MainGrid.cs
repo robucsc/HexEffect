8c3d415790a145b2d84eec29c7de8b2fdecee517
robucsc <robucsc@gmail.com>
2021-03-11 23:13:12 -0800

    Updated art, background, figured out more of the hex code,

---
+++ RoboEffect/Assets/MainGrid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainGrid : MonoBehaviour
+{
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GridLayout gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+        // Vector3Int cellPosition = gridLayout.WorldToCell(transform.position);
+        // transform.position = gridLayout.CellToWorld(cellPosition);
+        //
+        // Debug.Log(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // DoMouse();
+    }
+    
+    
+    // public class DoMouse() {
+    //     private Vector2 mousePos;
+    //     
+    //     void OnMouseDown()
+    //     {
+    //         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //         
+    //         Debug.Log(mousePos);
+    //
+    //     }
+    // }
+}
--- RoboEffect/Assets/MouseScript.cs
+++ RoboEffect/Assets/MouseScript.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MouseScript : MonoBehaviour
 {
+
+    private Tilemap tilemap;
     private bool isDragging;
     private Vector2 difference;
+    private Vector2 mousePos;
+    private Vector3Int mouseXYZpos;
+
+    private TileBase tile;
     
     public void OnMouseDown(){
         Debug.Log("mouse down");
@@ -21,12 +28,32 @@ public class MouseScript : MonoBehaviour
         isDragging = false;
     }
 
+    private void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
     private void Update()
     {
         if (isDragging)
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             transform.Translate(mousePos - difference);
         }
+        
+        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        // Vector3Int position = tilemap.WorldToCell(worldPoint);
+        //
+        // TileBase tile = tilemap.GetTile(position);
+        
+        
+        mouseXYZpos = new Vector3Int((int)Input.mousePosition.x, (int)Input.mousePosition.y , 0 );
+        Debug.Log(mouseXYZpos);
+        tile = tilemap.GetTile(mouseXYZpos);
+        
+         // Debug.Log(tile);
+        
     }
 }
+
